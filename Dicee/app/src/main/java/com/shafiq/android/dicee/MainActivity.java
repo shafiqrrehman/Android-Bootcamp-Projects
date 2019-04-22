6353cb7a372317868cf4029974c94f9f915652d4
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2019-04-22 14:10:44 +0500

    Linked xml layout with java & Dicee app up and running.

--- Dicee/app/src/main/java/com/shafiq/android/dicee/MainActivity.java
+++ Dicee/app/src/main/java/com/shafiq/android/dicee/MainActivity.java
@@ -2,6 +2,11 @@ package com.shafiq.android.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button mRollDice = findViewById(R.id.rollButton);
+
+        final ImageView mLeftDice = findViewById(R.id.image_leftDice);
+        final ImageView mRightDice = findViewById(R.id.image_rightDice);
+
+        final int[] mDiceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        mRollDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomGeneratedNumber = new Random();
+
+                int number = randomGeneratedNumber.nextInt(6);
+                mLeftDice.setImageResource(mDiceArray[number]);
+
+                number = randomGeneratedNumber.nextInt(6);
+                mRightDice.setImageResource(mDiceArray[number]);
+            }
+        });
     }
 }
